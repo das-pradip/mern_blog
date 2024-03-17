0c564dc8953cdd47b2f06f8de66a93c30509e9cd
PRADIP DAS <techdas199@gmail.com>
2024-03-17 11:03:35 +0530

    add OAuth functionality

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -15,6 +15,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    profilePicture:{
+        type: String,
+        default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAd5avdba8EiOZH8lmV3XshrXx7dKRZvhx-A&usqp=CAU",
+    },
 }, {timestamps: true})
 
 const User = mongoose.model("User", userSchema);
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -4,6 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { signInFailure, signInStart, signInSuccess} from "../redux/user/userSlice";
+import OAuth from "../components/OAuth";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
@@ -93,6 +94,7 @@ export default function SignIn() {
                   ) : 'Sign In'
                 }
               </Button>
+              <OAuth/>
             </form>
             <div className="flex gap-2 text-sm mt-5">
               <span>Dont have an account?</span>
