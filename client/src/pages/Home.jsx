b3900eb6873cfa321f02b507e6cd2856af322e3e
PRADIP DAS <techdas199@gmail.com>
2024-06-26 12:29:08 +0530

    fixied the home page from my end

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -31,12 +31,12 @@ export default function Home() {
       <div className="p-3 bg-amber-100 dark:bg-slate-700">
         <CallToAction />
       </div>
-      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
+      <div className=" max-w-8xl mx-auto p-3 flex flex-col gap-8 py-7">
            {
             posts && posts.length > 0 && (
               <div className="flex flex-col gap-6">
                   <h2 className="text-2xl font-semibold text-center">Recent Posts</h2>
-                  <div className="flex flex-wrap justify-center gap-4 md:mx-auto">
+                  <div className="flex flex-wrap justify-center gap-4 ">
                     {posts.map((post) => (
                         <PostCard key={posts._id} post={post} />
                     ))}
